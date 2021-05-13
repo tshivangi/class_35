624f5e1e98d5e00b887a8e7225b13f494c5e0fe3
tshivangi <83405123+tshivangi@users.noreply.github.com>
2021-05-13 05:41:10 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+//Create variables here
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey= "AIzaSyAVf04Q26Z8bmCLM6pN3FWZtUWlrR0NSH4",
+    authDomain: "class36-6d21c.firebaseapp.com",
+    projectId: "class36-6d21c",
+    storageBucket: "class36-6d21c.appspot.com",
+    messagingSenderId: "697202975321",
+    appId: "1:697202975321:web:5ec182b19bfe24ebfe0748"
+     }
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
+
+
+
